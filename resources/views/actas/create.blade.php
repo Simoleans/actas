b760ed941e28bae46a7f42ea4aa5c648d678d3ea
Francisco Hernandez <francisco20990@gmail.com>
2019-06-24 09:02:47 -0400

    Ordenar Codigo, modal registrar actas

--- resources/views/actas/create.blade.php
+++ resources/views/actas/create.blade.php
@@ -141,6 +141,31 @@
 			</div>
 		</div>
 	</div>
+
+
+
+
+
+	<!-- Modal -->
+<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      {{-- <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div> --}}
+      <div class="modal-body">
+        <h3 id="text_response" class="text-center"></h3>
+      </div>
+      {{-- <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Save changes</button>
+      </div> --}}
+    </div>
+  </div>
+</div>
 @endsection
 
 @section('script')
@@ -166,8 +191,9 @@
 	$(document).ready(function(){
 		//registrar todo el fomulario
 			$("#form_pad").submit(function(e){
-				e.preventDefault();		
-			//alert("fdfdfd")
+				e.preventDefault();	
+				$('#exampleModal').modal('toggle');	
+				$("#text_response").text("Guardando...");
 			var formData = new FormData($("#form_pad")[0]);
 				$.ajax({
 					url: '{{route('actas.store')}}',
@@ -178,7 +204,9 @@
 			        cache: false,
 			        processData:false,
 					success: function (response) {
-						alert(response.msg);
+						//alert(response.msg);
+						$("#text_response").text(response.msg);
+						$('#exampleModal').modal('hide');	
 					   window.location.replace(response.url);
 					},
 				});	
