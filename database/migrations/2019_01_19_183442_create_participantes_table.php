f2c3403be1393527a8f20523791f28834787ebd8
Fran <francisco20990@gmail.com>
2019-06-29 08:13:09 -0400

    clientes migrations

--- database/migrations/2019_01_19_183442_create_participantes_table.php
+++ database/migrations/2019_01_19_183442_create_participantes_table.php
@@ -1,8 +1,8 @@
 <?php
 
-use Illuminate\Support\Facades\Schema;
-use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
 
 class CreateParticipantesTable extends Migration
 {
@@ -16,11 +16,17 @@ class CreateParticipantesTable extends Migration
         Schema::create('participantes', function (Blueprint $table) {
             $table->increments('id');
             $table->string('codigo_acta'); //acta id
+            $table->integer('id_empresa')->unsigned(); //De uno a uno,  una empresa
+            $table->foreign('id_empresa')->references('id')->on('empresas')->onDelete('cascade');
             $table->string('nombre');
             $table->string('apellido');
+            $table->string('rut');
             $table->string('cargo');
             $table->string('email');
+            $table->string('telefono');
+            $table->string('direccion');
             $table->string('firma')->nullable();
+            $table->integer('status');
             $table->timestamps();
         });
     }
