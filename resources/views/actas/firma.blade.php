e748e2a4692d8043dd7a5b83b127b8ad1442bd3a
Francisco Hernandez <francisco20990@gmail.com>
2019-06-30 09:55:04 -0400

    Signature pad modificdo

--- resources/views/actas/firma.blade.php
+++ resources/views/actas/firma.blade.php
@@ -22,31 +22,90 @@
   border-radius: 0;
 }
 
-#sign-pad{
-   width:345px !important;
+
+.signature-pad {
+  position: relative;
+  display: -webkit-box;
+  display: -ms-flexbox;
+  display: flex;
+  -webkit-box-orient: vertical;
+  -webkit-box-direction: normal;
+      -ms-flex-direction: column;
+          flex-direction: column;
+  font-size: 10px;
+  width: 100%;
+  height: 100%;
+  /*max-width: 700px;*/
+  max-height: 460px;
+  border: 1px solid #e8e8e8;
+  background-color: #fff;
+  box-shadow: 0 1px 4px rgba(0, 0, 0, 0.27), 0 0 40px rgba(0, 0, 0, 0.08) inset;
+  border-radius: 4px;
+  padding: 16px;
 }
 
-#signArea{
-  width:404px;
-  margin: 50px auto;
-}
-.sign-container {
-  width: 60%;
-  margin: auto;
-}
-.sign-preview {
-  width: 250px;
-  height: 50px;
-  border: solid 1px #CFCFCF;
-  margin: 10px 5px;
-}
-.tag-ingo {
-  font-family: cursive;
-  font-size: 12px;
-  text-align: left;
-  font-style: oblique;
+.signature-pad::before,
+.signature-pad::after {
+  position: absolute;
+  z-index: -1;
+  content: "";
+  width: 40%;
+  height: 10px;
+  bottom: 10px;
+  background: transparent;
+  box-shadow: 0 8px 12px rgba(0, 0, 0, 0.4);
 }
 
+.signature-pad::before {
+  left: 20px;
+  -webkit-transform: skew(-3deg) rotate(-3deg);
+          transform: skew(-3deg) rotate(-3deg);
+}
+
+.signature-pad::after {
+  right: 20px;
+  -webkit-transform: skew(3deg) rotate(3deg);
+          transform: skew(3deg) rotate(3deg);
+}
+
+.signature-pad--body {
+  position: relative;
+  -webkit-box-flex: 1;
+      -ms-flex: 1;
+          flex: 1;
+  border: 1px solid #f4f4f4;
+}
+
+.signature-pad--body
+canvas {
+/*  position: absolute;*/
+  left: 0;
+  top: 0;
+  width: 100%;
+  height: 100%;
+  border-radius: 4px;
+  box-shadow: 0 0 5px rgba(0, 0, 0, 0.02) inset;
+}
+
+.signature-pad--footer {
+  color: #C3C3C3;
+  text-align: center;
+  font-size: 1.2em;
+  margin-top: 8px;
+}
+
+.signature-pad--actions {
+  display: -webkit-box;
+  display: -ms-flexbox;
+  display: flex;
+  -webkit-box-pack: justify;
+      -ms-flex-pack: justify;
+          justify-content: space-between;
+  margin-top: 8px;
+}
+
+
+}
 
 </style>
 
@@ -107,6 +166,34 @@
             </a>
            </div>
          </div>
+         <form method="POST" enctype="multipart/form-data" id="form_pad">
+         <meta name="csrf-token" content="{{ csrf_token() }}" />
+          <input type="hidden" name="id_participante" value="{{$participante->id}}">
+          <input type="hidden" name="firma" id="firma" required>
+          <div id="signature-pad" class="signature-pad">
+                  <div class="signature-pad--body" id="signatura-pad-image">
+                    <canvas></canvas>
+                  </div>
+                  <div class="signature-pad--footer">
+                    <div class="description">VeanX Technology<br>{{$participante->clientes->nombre.' '.$participante->clientes->apellido}}</div>
+
+                    <div class="signature-pad--actions">
+                      <div>
+                        <button type="button" class="button clear btn-lg btn-warning" data-action="clear">Limpiar</button>
+                        <button type="button" class="button" style="display: none;" data-action="change-color">Cambiar Color</button>
+                        <button type="button" class="button" style="display: none;" data-action="undo">corregir</button>
+
+                      </div>
+                      <div>
+                        <button type="button" class="button save bt btn-lg btn-success"  data-action="save-png">Guardar</button>
+                        <button type="button" class="button save" style="display: none;" data-action="save-jpg">Guardar en JPG</button>
+                        <button type="button" class="button save" style="display: none;" data-action="save-svg">Guardar en SVG</button>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+        </form>
+        
          @endif
 
       <br>
@@ -159,15 +246,9 @@
           <input type="hidden" name="firma" id="firma" required>
           <div class="row">
              @if(!$participante->firma)
-               <div class="col-md-6 col-md-offset-3">
+               <div class="col-md-12">
                   {{-- <label class="control-label" for="Firma">Firma: *</label> --}}
-                <div id="signArea">
-                  <div class="sig sigWrapper" style="height:auto;">
-                    <div class="typed"></div>
-                    <canvas class="sign-pad" id="sign-pad"></canvas>
-                  </div>
-                  <h3 class="tag-ingo text-center">{{$participante->nombre.' '.$participante->apellido}}</h3>
-                </div>
+                 
               </div>
               <div class="row">
               <div class="form-group text-center">
@@ -195,26 +276,158 @@
 
   $(document).ready(function(){
 
-    $("#clear").click(function(e){
-       $('#signArea').signaturePad().clearCanvas();
-    });
+    // console.log("inciio")
 
-      //firma
-      $('#signArea').signaturePad({drawOnly:true, drawBezierCurves:true, lineTop:90});
+    // $("#clear").click(function(e){
+    //    $('#signArea').signaturePad().clearCanvas();
+    // });
 
-      $("#form_pad").submit(function(e){
-        e.preventDefault();
-        html2canvas([document.getElementById('sign-pad')], {
+    //   //firma
+    //   $('#signArea').signaturePad({drawOnly:true, drawBezierCurves:true, lineTop:90});
+
+    //   $("#form_pad").submit(function(e){
+    //     e.preventDefault();
+    //     html2canvas([document.getElementById('sign-pad')], {
+    //       onrendered: function (canvas) {
+    //         var canvas_img_data = canvas.toDataURL('image/png');
+    //         var img_data = canvas_img_data.replace(/^data:image\/(png|jpg);base64,/, "");
+
+    //         $("#firma").val(img_data);
+
+    //         if ( $("p.error").is(':visible') ) {
+    //           $("p.error").text("Falta la firma del documento.");
+
+    //         }else{
+    //             $.ajax({
+    //               headers: {
+    //                   'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+    //               },
+    //               url: '{{route('actas.send')}}',
+    //               data: $("#form_pad").serialize(),
+    //               type: 'post',
+    //               dataType: 'json',
+    //               success: function (response) {
+    //                 alert(response.msg);
+    //                 $('#myModal').modal('hide');
+    //                  window.location.reload();
+    //               }
+    //             });
+    //         }
+
+
+    //       }
+    //     });// fin html2canvas
+    //   }); //fin firma
+ var wrapper = document.getElementById("signature-pad");
+var clearButton = wrapper.querySelector("[data-action=clear]");
+var changeColorButton = wrapper.querySelector("[data-action=change-color]");
+var undoButton = wrapper.querySelector("[data-action=undo]");
+var savePNGButton = wrapper.querySelector("[data-action=save-png]");
+var saveJPGButton = wrapper.querySelector("[data-action=save-jpg]");
+var saveSVGButton = wrapper.querySelector("[data-action=save-svg]");
+var canvas = wrapper.querySelector("canvas");
+var signaturePad = new SignaturePad(canvas, {
+  // It's Necessary to use an opaque color when saving image as JPEG;
+  // this option can be omitted if only saving as PNG or SVG
+  backgroundColor: 'rgb(255, 255, 255)'
+});
+
+console.log(wrapper)
+
+// Adjust canvas coordinate space taking into account pixel ratio,
+// to make it look crisp on mobile devices.
+// This also causes canvas to be cleared.
+function resizeCanvas() {
+  // When zoomed out to less than 100%, for some very strange reason,
+  // some browsers report devicePixelRatio as less than 1
+  // and only part of the canvas is cleared then.
+  var ratio =  Math.max(window.devicePixelRatio || 1, 1);
+
+  // This part causes the canvas to be cleared
+  canvas.width = canvas.offsetWidth * ratio;
+  canvas.height = canvas.offsetHeight * ratio;
+  canvas.getContext("2d").scale(ratio, ratio);
+
+  // This library does not listen for canvas changes, so after the canvas is automatically
+  // cleared by the browser, SignaturePad#isEmpty might still return false, even though the
+  // canvas looks empty, because the internal data of this library wasn't cleared. To make sure
+  // that the state of this library is consistent with visual state of the canvas, you
+  // have to clear it manually.
+  signaturePad.clear();
+}
+
+// On mobile devices it might make more sense to listen to orientation change,
+// rather than window resize events.
+window.onresize = resizeCanvas;
+resizeCanvas();
+
+function download(dataURL, filename) {
+  if (navigator.userAgent.indexOf("Safari") > -1 && navigator.userAgent.indexOf("Chrome") === -1) {
+    window.open(dataURL);
+  } else {
+    var blob = dataURLToBlob(dataURL);
+    var url = window.URL.createObjectURL(blob);
+
+    var a = document.createElement("a");
+    a.style = "display: none";
+    a.href = url;
+    a.download = filename;
+
+    document.body.appendChild(a);
+    a.click();
+
+    window.URL.revokeObjectURL(url);
+  }
+}
+
+// One could simply use Canvas#toBlob method instead, but it's just to show
+// that it can be done using result of SignaturePad#toDataURL.
+function dataURLToBlob(dataURL) {
+  // Code taken from https://github.com/ebidel/filer.js
+  var parts = dataURL.split(';base64,');
+  var contentType = parts[0].split(":")[1];
+  var raw = window.atob(parts[1]);
+  var rawLength = raw.length;
+  var uInt8Array = new Uint8Array(rawLength);
+
+  for (var i = 0; i < rawLength; ++i) {
+    uInt8Array[i] = raw.charCodeAt(i);
+  }
+
+  return new Blob([uInt8Array], { type: contentType });
+}
+
+clearButton.addEventListener("click", function (event) {
+  signaturePad.clear();
+});
+
+undoButton.addEventListener("click", function (event) {
+  var data = signaturePad.toData();
+
+  if (data) {
+    data.pop(); // remove the last dot or line
+    signaturePad.fromData(data);
+  }
+});
+
+var toogle=true;
+
+
+
+savePNGButton.addEventListener("click", function (event) {
+  if (signaturePad.isEmpty()) {
+    swal("Debe poner una firma.");
+  } else {
+    // var dataURL = signaturePad.toDataURL();
+    // download(dataURL, "signature.png");
+
+    html2canvas([document.getElementById('signatura-pad-image')], {
           onrendered: function (canvas) {
             var canvas_img_data = canvas.toDataURL('image/png');
             var img_data = canvas_img_data.replace(/^data:image\/(png|jpg);base64,/, "");
 
             $("#firma").val(img_data);
 
-            if ( $("p.error").is(':visible') ) {
-              $("p.error").text("Falta la firma del documento.");
-
-            }else{
                 $.ajax({
                   headers: {
                       'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
@@ -224,17 +437,23 @@
                   type: 'post',
                   dataType: 'json',
                   success: function (response) {
-                    alert(response.msg);
+                    swal(response.msg);
                     $('#myModal').modal('hide');
                      window.location.reload();
                   }
                 });
-            }
+            
 
 
           }
         });// fin html2canvas
-      }); //fin firma
+  }
+});
+
+
+
+
+
 
 });
 </script>
